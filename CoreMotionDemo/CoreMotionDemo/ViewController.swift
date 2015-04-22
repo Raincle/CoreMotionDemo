f1844f99e60bdd55f834c5f6f76d9bb1bad371aa
Raincle <yaweihong@icloud.com>
2015-04-23 02:13:01 +0800

    output

--- CoreMotionDemo/CoreMotionDemo/ViewController.swift
+++ CoreMotionDemo/CoreMotionDemo/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var aMaxX: UILabel!
     @IBOutlet weak var aMaxY: UILabel!
     @IBOutlet weak var aMaxZ: UILabel!
+    var AMX:Double!
+    var AMY:Double!
+    var AMZ:Double!
     
     @IBOutlet weak var rLabelX: UILabel!
     @IBOutlet weak var rLabelY: UILabel!
@@ -27,10 +30,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var rMaxX: UILabel!
     @IBOutlet weak var rMaxY: UILabel!
     @IBOutlet weak var rMaxZ: UILabel!
+    var RMX:Double!
+    var RMY:Double!
+    var RMZ:Double!
+    
+    var motionManager = CMMotionManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        AMX = 0
+        AMY = 0
+        AMZ = 0
+        RMX = 0
+        RMY = 0
+        RMZ = 0
+        
         // Do any additional setup after loading the view, typically from a nib.
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.gyroUpdateInterval = 0.2
+    
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()) {
+            acceData, error in
+            
+            if (error != nil) {
+                println("Error: \(error)")
+            }
+            self.outputA(acceData.acceleration)
+        }
+        
+        motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()) {
+            gyroData, error in
+            
+            if (error != nil) {
+                println("Error: \(error)")
+            }
+            self.outputR(gyroData.rotationRate)
+        }
+        
+        
+        
+    }
+    
+    func outputA(data:CMAcceleration) {
+
+        aLabelX.text = "\(data.x)"
+        if fabs(data.x) > AMX {
+            AMX = fabs(data.x)
+            aMaxX.text = "\(AMX)"
+        }
+        
+        aLabelY.text = "\(data.y)"
+        if fabs(data.y) > AMY {
+            AMY = fabs(data.y)
+            aMaxY.text = "\(AMY)"
+        }
+        
+        aLabelZ.text = "\(data.z)"
+        if fabs(data.z) > AMZ {
+            AMZ = fabs(data.z)
+            aMaxZ.text = "\(AMZ)"
+        }
+    }
+    
+    func outputR(data:CMRotationRate) {
+        
+        rLabelX.text = "\(data.x)"
+        if fabs(data.x) > RMX {
+            RMX = fabs(data.x)
+            rMaxX.text = "\(RMX)"
+        }
+        
+        rLabelY.text = "\(data.y)"
+        if fabs(data.y) > RMY {
+            RMY = fabs(data.y)
+            rMaxY.text = "\(RMY)"
+        }
+        
+        rLabelZ.text = "\(data.z)"
+        if fabs(data.z) > RMZ {
+            RMZ = fabs(data.z)
+            rMaxZ.text = "\(RMZ)"
+        }
     }
 
     override func didReceiveMemoryWarning() {
