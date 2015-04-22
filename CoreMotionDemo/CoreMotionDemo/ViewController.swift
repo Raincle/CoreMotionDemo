6d17886f9617c6f6b1b743bd71a25f1a34d6a291
Raincle <yaweihong@icloud.com>
2015-04-22 22:59:38 +0800

    accelerometer UI

--- CoreMotionDemo/CoreMotionDemo/ViewController.swift
+++ CoreMotionDemo/CoreMotionDemo/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
