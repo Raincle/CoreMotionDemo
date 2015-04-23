10fcd44cdc6146a525f3b420f6a31ec7ffd1d8f8
Raincle <yaweihong@icloud.com>
2015-04-23 13:35:12 +0800

    Finished

--- CoreMotionDemo/CoreMotionDemo/ViewController.swift
+++ CoreMotionDemo/CoreMotionDemo/ViewController.swift
@@ -11,6 +11,7 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var updateRate: UILabel!
 
     @IBOutlet weak var aLabelX: UILabel!
     @IBOutlet weak var aLabelY: UILabel!
@@ -35,6 +36,7 @@ class ViewController: UIViewController {
     var RMZ:Double!
     
     var motionManager = CMMotionManager()
+    var isStart = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,70 +49,52 @@ class ViewController: UIViewController {
         RMZ = 0
         
         // Do any additional setup after loading the view, typically from a nib.
-        motionManager.accelerometerUpdateInterval = 0.2
-        motionManager.gyroUpdateInterval = 0.2
+        motionManager.accelerometerUpdateInterval = 0.5
+        motionManager.gyroUpdateInterval = 0.5
     
-        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()) {
-            acceData, error in
-            
-            if (error != nil) {
-                println("Error: \(error)")
-            }
-            self.outputA(acceData.acceleration)
-        }
-        
-        motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()) {
-            gyroData, error in
-            
-            if (error != nil) {
-                println("Error: \(error)")
-            }
-            self.outputR(gyroData.rotationRate)
-        }
-        
-        
+
         
     }
     
     func outputA(data:CMAcceleration) {
 
-        aLabelX.text = "\(data.x)"
+        aLabelX.text = String(format: "%.2f", data.x)
         if fabs(data.x) > AMX {
             AMX = fabs(data.x)
-            aMaxX.text = "\(AMX)"
+            aMaxX.text = String(format: "%.2f", AMX)
         }
         
-        aLabelY.text = "\(data.y)"
+        aLabelY.text = String(format: "%.2f", data.y)
         if fabs(data.y) > AMY {
             AMY = fabs(data.y)
-            aMaxY.text = "\(AMY)"
+            aMaxY.text = String(format: "%.2f", AMY)
         }
         
-        aLabelZ.text = "\(data.z)"
+        aLabelZ.text = String(format: "%.2f", data.z)
         if fabs(data.z) > AMZ {
             AMZ = fabs(data.z)
-            aMaxZ.text = "\(AMZ)"
+            aMaxZ.text = String(format: "%.2f", AMZ)
         }
     }
     
     func outputR(data:CMRotationRate) {
         
-        rLabelX.text = "\(data.x)"
+        rLabelX.text = String(format: "%.2f", data.x)
         if fabs(data.x) > RMX {
             RMX = fabs(data.x)
-            rMaxX.text = "\(RMX)"
+            rMaxX.text = String(format: "%.2f", RMX)
         }
         
-        rLabelY.text = "\(data.y)"
+        rLabelY.text = String(format: "%.2f", data.y)
         if fabs(data.y) > RMY {
             RMY = fabs(data.y)
-            rMaxY.text = "\(RMY)"
+            rMaxY.text = String(format: "%.2f", RMY)
         }
         
-        rLabelZ.text = "\(data.z)"
+        rLabelZ.text = String(format: "%.2f", data.z)
         if fabs(data.z) > RMZ {
             RMZ = fabs(data.z)
-            rMaxZ.text = "\(RMZ)"
+            rMaxZ.text = String(format: "%.2f", RMZ)
         }
     }
 
@@ -120,14 +104,73 @@ class ViewController: UIViewController {
     }
 
     @IBAction func updateRate(sender: UISlider) {
+        updateRate.text = String(format: "%.2f", sender.value)
+        motionManager.accelerometerUpdateInterval = Double(sender.value)
+        motionManager.gyroUpdateInterval = Double(sender.value)
     }
 
     
-    @IBAction func onSwitch(sender: AnyObject) {
+
+    @IBAction func onSwitch(sender: UIButton) {
+        if !isStart {
+            isStart = true
+            sender.setTitle("Stop", forState: UIControlState.Normal)
+            
+            if isStart {
+                motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()) {
+                    acceData, error in
+                    
+                    if (error != nil) {
+                        println("Error: \(error)")
+                    }
+                    self.outputA(acceData.acceleration)
+                }
+                
+                motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()) {
+                    gyroData, error in
+                    
+                    if (error != nil) {
+                        println("Error: \(error)")
+                    }
+                    self.outputR(gyroData.rotationRate)
+                }
+            }
+            
+        } else {
+            isStart = false
+            sender.setTitle("Start", forState: UIControlState.Normal)
+            motionManager.stopAccelerometerUpdates()
+            motionManager.stopGyroUpdates()
+        }
     }
 
     
     @IBAction func reset(sender: AnyObject) {
+        
+        AMX = 0
+        AMY = 0
+        AMZ = 0
+        RMX = 0
+        RMY = 0
+        RMZ = 0
+        
+        aMaxX.text = "0"
+        aMaxY.text = "0"
+        aMaxZ.text = "0"
+        rMaxX.text = "0"
+        rMaxY.text = "0"
+        rMaxZ.text = "0"
+        
+        rLabelX.text = "0"
+        rLabelY.text = "0"
+        rLabelZ.text = "0"
+        aLabelX.text = "0"
+        aLabelY.text = "0"
+        aLabelZ.text = "0"
+        
+        
+
+        
     }
 }
 
